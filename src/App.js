7cf6a8bc35eb0b35dbfa9641e108e4fa0e535930
Ming-Zhao Shi <shim@xceed.com>
2020-02-19 16:36:54 -0500

    add ag-grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-enterprise";
+
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-balham.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columnDefs: [
+        {
+          headerName: "Make",
+          field: "make",
+          rowGroup: true
+        },
+        {
+          headerName: "Price",
+          field: "price"
+        }
+      ],
+      autoGroupColumnDef: {
+        headerName: "Model",
+        field: "model",
+        cellRenderer: "agGroupCellRenderer",
+        cellRendererParams: {
+          checkbox: true
+        }
+      }
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.myjson.com/bins/ly7d1")
+      .then(result => result.json())
+      .then(rowData => this.setState({ rowData }));
+  }
+
+  onButtonClick = e => {
+    const selectedNodes = this.gridApi.getSelectedNodes();
+    const selectedData = selectedNodes.map(node => node.data);
+    const selectedDataStringPresentation = selectedData
+      .map(node => node.make + " " + node.model)
+      .join(", ");
+    alert(`Selected nodes: ${selectedDataStringPresentation}`);
+  };
+
+  render() {
+    return (
+      <div
+        className="ag-theme-balham"
+        style={{ height: "400px", width: "600px" }}
+      >
+        <button onClick={this.onButtonClick}>Get selected rows</button>
+        <AgGridReact
+          columnDefs={this.state.columnDefs}
+          groupSelectsChildren={true}
+          autoGroupColumnDef={this.state.autoGroupColumnDef}
+          rowData={this.state.rowData}
+          rowSelection="multiple"
+          onGridReady={params => (this.gridApi = params.api)}
+        ></AgGridReact>
+      </div>
+    );
+  }
 }
 
 export default App;
